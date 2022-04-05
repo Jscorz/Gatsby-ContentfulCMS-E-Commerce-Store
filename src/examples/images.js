78df18f2d405d7ddb5d4ae526ed903127053ffe6
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-05 13:12:52 -0600

    added staticImage

--- src/examples/images.js
+++ src/examples/images.js
@@ -1,9 +1,11 @@
 import React from "react"
 import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
+      <StaticImage src="../assets/images/about.jpeg" alt="example" />
       <h2>gatsby image</h2>
     </Wrapper>
   )
