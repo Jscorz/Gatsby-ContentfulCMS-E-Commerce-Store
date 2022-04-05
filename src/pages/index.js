5d6955d99be42774d23ecb83a99404c46682e332
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-05 09:08:58 -0600

    changed h1 into seperate classes for h1s

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,11 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
-      <h1 style={{ color: "red", textTransform: "capitalize" }}>home page</h1>
-      <div>
-        <Link to="/about">about</Link>
+      <div className="home-heading">
+        <h1>home page</h1>
+        <h1>hello world</h1>
+        <h1>hello people</h1>
       </div>
-      <div>
-        <Link to="/company/history">history</Link>
-      </div>
-      <a href="/about">regular link</a>
-      <a href="https://www.gatsbyjs.com/docs/how-to/" target="_blank">
-        gatsby docs
-      </a>
     </Layout>
   )
 }
