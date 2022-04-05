6ed0a80520c9d1c51c09c3a11ddc96752296d4ff
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-05 11:17:10 -0600

    built out footer page

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const Recipes = () => {
-  return <div>recipes page</div>
+  return (
+    <Layout>
+      <h1>Recipes page</h1>
+    </Layout>
+  )
 }
 
 export default Recipes
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const Tags = () => {
-  return <div>tags page</div>
+  return (
+    <Layout>
+      <h1>Tags page</h1>
+    </Layout>
+  )
 }
 
 export default Tags
