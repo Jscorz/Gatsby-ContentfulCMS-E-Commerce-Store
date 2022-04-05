bdc7350a8183b30cc8c70e50334def8aff536dec
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-05 14:43:40 -0600

    added image to about page and built out text elements

--- src/examples/images.js
+++ src/examples/images.js
@@ -55,10 +55,14 @@ const Wrapper = styled.section`
   }
   .example-img {
     border-radius: 1rem;
+    height: 300px;
   }
-  /* @media (min-width: 992px) {
+  @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr 1fr;
-  } */
+    .example-img {
+      height: 200px;
+    }
+  }
 `
 
 export default Images
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,36 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const about = () => {
   return (
     <Layout>
-      <h1>about page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Vexillologist kinfolk pok pok. </h2>
+            <p>
+              Edison bulb truffaut distillery, hella vice polaroid actually
+              shabby chic tumblr fanny pack irony unicorn brooklynbelly kinfolk
+              next level live-edge.
+            </p>
+            <p>
+              Tattooed whatever blog meditation hashtag 3 wolf moon flannel
+              godard adaptogen.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
