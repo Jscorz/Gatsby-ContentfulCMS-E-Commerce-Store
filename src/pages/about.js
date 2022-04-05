f283bea3ffadc860aac3e98d1de86980b132fc90
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-05 09:17:21 -0600

    started building css modules for home and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
         <h1>about page</h1>
+        <h1>hello world</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ad ex,
+          numquam necessitatibus voluptates repellat iure dolores dolorem unde
+          recusandae praesentium qui molestiae, quaerat quasi? Ratione, aliquam
+          tenetur? Voluptas libero porro voluptatum veniam, aliquam tempora
+          assumenda facilis minus blanditiis amet!
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples//home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          debitis libero atque maxime quae laborum facilis qui voluptatum itaque
+          nemo eveniet quas, animi tempora, tenetur minus unde corporis optio
+          repudiandae! Ea vel, pariatur nesciunt nulla culpa ipsam obcaecati
+          voluptate explicabo!
+        </p>
       </div>
     </Layout>
   )
