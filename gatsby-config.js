282e94a34de812996c30ca5a26092309d5ef7ddf
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-06 09:14:47 -0600

    started adding siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,19 @@
 
 module.exports = {
   /* Your site config here */
+
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@johndoe",
+    person: { name: "John, age:28" },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 22 },
+    ],
+  },
+
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
