c697e9d8af2f6463f279e2bf07c7adc22d304ca3
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-06 16:32:05 -0600

    added gatsby-source-contentful to plugins and built development and production env keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
 
@@ -29,6 +33,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `p5tl8d8f7yf9`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
   ],
