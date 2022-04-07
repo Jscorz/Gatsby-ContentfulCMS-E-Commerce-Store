0396eef3dbae36e80c127828ad658ad87aafd8f5
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 15:02:12 -0600

    added forEach to iterate over nodes to create dynamic path ending in tag variable

---
+++ gatsby-node.js
@@ -0,0 +1,23 @@
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query GetRecipes {
+      allContentfulRecipe {
+        nodes {
+          content {
+            tags
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allContentfulRecipe.nodes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      createPage({
+        path: `/${tag}`,
+      })
+    })
+  })
+}
