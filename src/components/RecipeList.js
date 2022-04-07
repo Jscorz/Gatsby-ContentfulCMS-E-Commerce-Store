c4c1a8408bf88c6152c5765b9be5119b245c5a43
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 08:33:20 -0600

    started destructuring recipes query in RecipeList component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-const RecipeList = () => {
+const RecipeList = ({ recipes = [] }) => {
   return (
-    <div>
+    <div className="recipes-list">
+      {recipes.map(recipe => {
+        return <p>{recipe.title}</p>
+      })}
       <h4>recipe list</h4>
     </div>
   )
