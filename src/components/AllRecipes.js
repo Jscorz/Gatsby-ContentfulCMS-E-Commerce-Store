6ec397e8e21c50808cbc6b1e32697c3f817a2955
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 08:29:23 -0600

    started building out AllRecipes,RecipeList,TagsList components and setup the graphql query in AllRecipes and passed down data to TagsList and RecipeList comps as props

---
+++ src/components/AllRecipes.js
@@ -0,0 +1,37 @@
+import React from "react"
+import TagsList from "./TagsList"
+import RecipeList from "./RecipeList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
+
+const AllRecipes = () => {
+  const data = useStaticQuery(query)
+  const recipes = data.allContentfulRecipe.nodes
+
+  return (
+    <section className="recipes-container">
+      <TagsList recipes={recipes} />
+      <RecipeList recipes={recipes} />
+    </section>
+  )
+}
+
+export default AllRecipes
---
+++ src/components/RecipeList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RecipeList = () => {
+  return (
+    <div>
+      <h4>recipe list</h4>
+    </div>
+  )
+}
+
+export default RecipeList
