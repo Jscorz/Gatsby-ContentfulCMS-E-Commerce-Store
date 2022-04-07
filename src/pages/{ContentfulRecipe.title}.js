d671f49fa08673523951274df668cbdee9025d94
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 13:58:05 -0600

    setup icon sections with prep time cook time and servings

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { BsClockHistory, BsClock, BsPeople, BsTypeH2 } from "react-icons/bs"
+import Layout from "../components/Layout"
 
 const RecipeTemplate = ({ data }) => {
   const {
@@ -16,11 +17,55 @@ const RecipeTemplate = ({ data }) => {
   const pathToImage = getImage(image)
   const { tags, instructions, ingredients, tools } = content
   return (
-    <main className="page">
-      <div className="recipe-page">
-        <h2> {title}</h2>
-      </div>
-    </main>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          {/* hero */}
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* recipe-icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags :{" "}
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the content */}
+          <section className="recipe-content"></section>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
