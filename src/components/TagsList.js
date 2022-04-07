21dcd5360d622a6fd283f37ace1642aa7ac805de
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 11:33:16 -0600

    setupTags to create property for newTags and to add + 1 for existing tags, then destructuring array of two properties down to seperate arrays and compare with localeCompare

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -3,6 +3,7 @@ import setupTags from "../utils/setupTags"
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes)
+  console.log(newTags)
   return (
     <div>
       <h4>tags list </h4>
