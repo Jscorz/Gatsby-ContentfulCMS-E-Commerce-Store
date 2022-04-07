2835952fd260fa362299d83ecc614cb23c3f60d1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-07 15:55:52 -0600

    setup tags pages programatically with gatsby node js

--- src/template/tag-template.js
+++ src/template/tag-template.js
@@ -1,11 +1,40 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import RecipeList from "../components/RecipeList"
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes
+  console.log(recipes)
   return (
-    <div>
-      <h2>tag template page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.title}</h2>
+        <div className="tag-recipes">
+          <RecipeList recipes={recipes} />
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      filter: { content: { tags: { eq: $tag } } }
+      sort: { fields: title, order: ASC }
+    ) {
+      nodes {
+        title
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 export default TagTemplate
